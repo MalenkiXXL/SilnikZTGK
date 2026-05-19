e3a930df6ca76ba561366f8bc8c593f65bde316d
Amelia6554 <amelia.garnys@gmail.com>
2026-05-19 22:37:51 +0200

    feat: prefabs from files include relationship linking

--- SilnikZTGK/CookingStation/Scene/PrefabSerializer.h
+++ SilnikZTGK/CookingStation/Scene/PrefabSerializer.h
@@ -94,8 +94,18 @@ public:
         }
 
         // Parsowanie bezpośrednio z pamięci RAM (VFS)
-        nlohmann::json item = nlohmann::json::parse(fileData.begin(), fileData.end());
-        auto builder = scene->GetWorld().BuildEntity();
+        nlohmann::json parsedData = nlohmann::json::parse(fileData.begin(), fileData.end());
+        if (parsedData.is_object()) {
+            parsedData = nlohmann::json::array({ parsedData });
+        }
+
+        std::unordered_map<int, Entity> localIdToRealEntity;
+        std::unordered_map<std::size_t, Entity> rawIdToEntity;
+        Entity rootEntity = { std::numeric_limits<std::size_t>::max(), 0 };
+        auto& world = scene->GetWorld();
+
+        for (const auto& item : parsedData) {
+            auto builder = scene->GetWorld().BuildEntity();
 
         std::string name = item.contains("name") ? item["name"].get<std::string>() : "Prefab";
         builder.With<TagComponent>({ name });
@@ -104,6 +114,18 @@ public:
 
         // ZMIANA: Wgrywamy dane przez Settery, co automatycznie ustawi flag� m_IsDirty na true!
         transComp.SetPosition(spawnPos);
+
+        glm::vec3 localPos = { 0.0f, 0.0f, 0.0f };
+        if (item.contains("position")) {
+            localPos = { item["position"][0], item["position"][1], item["position"][2] };
+        }
+
+        if (item.contains("parent_id")) {
+            transComp.SetPosition(localPos);
+        }
+        else {
+            transComp.SetPosition(spawnPos + localPos);
+        }
 
         if (item.contains("rotation") && item.contains("scale")) {
             transComp.SetRotation({ item["rotation"][0], item["rotation"][1], item["rotation"][2] });
@@ -129,19 +151,80 @@ public:
 
         if (item.contains("scripts")) {
             NativeScriptComponent nsc;
-            // Iterujemy po liście w JSON i dodajemy do komponentu przez nasz nowy Rejestr
             for (const auto& scriptName : item["scripts"]) {
                 ScriptRegistry::AddScriptToComponent(nsc, scriptName.get<std::string>());
             }
             builder.With<NativeScriptComponent>(nsc);
         }
-        // Opcjonalnie: kompatybilność wsteczna z Twoimi starymi zapisami scen
         else if (item.contains("script")) {
             NativeScriptComponent nsc;
             ScriptRegistry::AddScriptToComponent(nsc, item["script"].get<std::string>());
             builder.With<NativeScriptComponent>(nsc);
         }
 
-        return builder.Build();
+        Entity newEntity = builder.Build();
+
+            if (item.contains("local_id")) {
+                localIdToRealEntity[item["local_id"].get<int>()] = newEntity;
+            }
+            rawIdToEntity[newEntity.id] = newEntity;
+
+            if (rootEntity.id == std::numeric_limits<std::size_t>::max()) {
+                rootEntity = newEntity;
+            }
+        }
+
+        // --- ZMIANA 6: Druga pętla budująca strukturę hierarchii (opcjonalna) ---
+        for (const auto& item : parsedData) {
+            if (item.contains("parent_id") && item.contains("local_id")) {
+                int localId = item["local_id"].get<int>();
+                int parentId = item["parent_id"].get<int>();
+
+                Entity child = localIdToRealEntity[localId];
+                Entity parent = localIdToRealEntity[parentId];
+
+                // 1. Zapewniamy istnienie komponentu Relationship na dziecku
+                auto* childRel = world.GetComponent<RelationshipComponent>(child);
+                if (!childRel) {
+                    world.AddComponent<RelationshipComponent>(child, RelationshipComponent{});
+                    childRel = world.GetComponent<RelationshipComponent>(child);
+                }
+
+                // 2. Zapewniamy istnienie komponentu Relationship na rodzicu
+                auto* parentRel = world.GetComponent<RelationshipComponent>(parent);
+                if (!parentRel) {
+                    world.AddComponent<RelationshipComponent>(parent, RelationshipComponent{});
+                    parentRel = world.GetComponent<RelationshipComponent>(parent);
+                }
+
+                // 3. Ustawiamy powiązanie w górę (dziecko -> rodzic)
+                childRel->Parent = parent.id;
+                parentRel->ChildrenCount++;
+
+                // 4. Ustawiamy powiązania w dół i w bok (rodzic -> dzieci, brat -> brat)
+                if (parentRel->FirstChild == NULL_ENTITY) {
+                    // Jeśli to pierwsze dziecko, przypisujemy je bezpośrednio do ojca
+                    parentRel->FirstChild = child.id;
+                }
+                else {
+                    std::size_t currSiblingId = parentRel->FirstChild;
+                    // Tłumaczymy surowe ID na pełną encję za pomocą naszej nowej mapy!
+                    Entity currSibling = rawIdToEntity[currSiblingId];
+                    auto* currSiblingRel = world.GetComponent<RelationshipComponent>(currSibling);
+
+                    while (currSiblingRel->NextSibling != NULL_ENTITY) {
+                        currSiblingId = currSiblingRel->NextSibling;
+                        currSibling = rawIdToEntity[currSiblingId];
+                        currSiblingRel = world.GetComponent<RelationshipComponent>(currSibling);
+                    }
+
+                    currSiblingRel->NextSibling = child.id;
+                    childRel->PreviousSibling = currSiblingId;
+                }
+            }
+        }
+
+        return rootEntity;
+
     } // Koniec funkcji Deserialize
 }; // Koniec klasy PrefabSerializer
