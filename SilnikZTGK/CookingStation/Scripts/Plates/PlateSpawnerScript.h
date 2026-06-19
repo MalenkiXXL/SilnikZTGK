549d9c870617687e67080df3a798b8da3971bb2e
ola <aleksandra.8753@gmail.com>
2026-06-19 11:11:05 +0200

    Plate stack shows up and lights up

--- SilnikZTGK/CookingStation/Scripts/Plates/PlateSpawnerScript.h
+++ SilnikZTGK/CookingStation/Scripts/Plates/PlateSpawnerScript.h
@@ -1,19 +1,25 @@
 #pragma once
 #include "CookingStation/Scene/ScriptableEntity.h"
 #include <string>
+#include <vector>
 
 class PlateSpawnerScript : public ScriptableEntity
 {
     float m_SpawnInterval = 3.0f;
     float m_TimeSinceLastSpawn = 0.0f;
     std::string m_PrefabPath = "assets://prefabs/plate.json";
-    int m_MaxPlates = 6;
+
     int m_ActivePlates = 0;
+    std::vector<Entity> m_VisualStack;
+    bool m_PendingGoldFlash = false;
 
 public:
     void OnCreate() override;
+    void OnDestroy() override;
     void OnUpdate(Timestep ts) override;
     void onPlateDeSpawned() { m_ActivePlates--; }
 private:
     void SpawnPrefab();
+    int CalculateMaxPlates();
+    void UpdateVisualStack(int targetCount);
 };
